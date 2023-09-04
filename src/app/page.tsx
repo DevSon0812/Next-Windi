c95c68d4b550bbffbded9b42f5e559f2a6045179
locbh3010 <locbh3010@gmail.com>
2023-09-05 02:49:52 +0700

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+'use client';
+
+import { Button } from '@/components/primitive';
+
 export default function Home() {
-    return <div></div>;
+    return (
+        <div className="h-screen w-screen flex items-center justify-center">
+            <Button
+                variant="text"
+                color="primary"
+                size="large"
+                textTransform="capitalize"
+            >
+                Click me
+            </Button>
+        </div>
+    );
 }
