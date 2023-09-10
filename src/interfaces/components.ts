e8b417dd89ecc5ea6439ec29c5dc1ac4e73c776f
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2023-09-11 06:53:52 +0700

    fix

--- src/interfaces/components.ts
+++
@@ -1,6 +0,0 @@
-export type ColorType = 'primary' | 'secondary' | 'success' | 'warning' | 'error' | 'info';
-export type SizeType = 'small' | 'medium' | 'large';
-export type RoundedType = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'none';
-export type TextTransformType = 'uppercase' | 'lowercase' | 'capitalize';
-
-export type ButtonVariantType = 'outlined' | 'contained' | 'text' | 'soft';
